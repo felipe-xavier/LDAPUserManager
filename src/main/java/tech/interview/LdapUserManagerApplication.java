6c25a3bc3dfeb91749c110442dc02ccf479d2f88
felipe.xavier <felipe.xavier@funceme.br>
2020-09-28 12:33:21 -0300

    First commit.

---
+++ src/main/java/tech/interview/LdapUserManagerApplication.java
@@ -0,0 +1,14 @@
+package tech.interview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LdapUserManagerApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(LdapUserManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/interview/LdapUserManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.interview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LdapUserManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
