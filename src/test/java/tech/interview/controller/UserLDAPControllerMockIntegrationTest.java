8fa8cbf19cc0ba2bd04fb3d4adea0ade2d0ea20b
felipe.xavier <felipe.xavier@funceme.br>
2020-10-01 15:37:42 -0300

    UserLDAPControllerMockIntegrationTest.java - Remove unnecessary dependency.

--- src/test/java/tech/interview/controller/UserLDAPControllerMockIntegrationTest.java
+++ src/test/java/tech/interview/controller/UserLDAPControllerMockIntegrationTest.java
@@ -18,7 +18,6 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
