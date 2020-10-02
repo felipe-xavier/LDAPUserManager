d732a6ffe22443a2589ffa316abf5c39102d385b
felipe.xavier <felipe.xavier@funceme.br>
2020-10-02 17:02:41 -0300

    Add Swagger Configuration.

---
+++ src/main/java/tech/interview/config/SwaggerConfig.java
@@ -0,0 +1,38 @@
+package tech.interview.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket LDAPUserAPI() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("tech.interview"))
+                .build()
+                .apiInfo(metaInfo());
+    }
+
+    private ApiInfo metaInfo() {
+        return new ApiInfoBuilder()
+                .title("LDAP User API")
+                .description("API REST for the LDAP User")
+                .version("1.0.0")
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"")
+                .contact(new Contact("Felipe de Almeida Xavier",
+                        "https://github.com/felipe-xavier",
+                        "felipe.uece@gmail.com"))
+                .build();
+    }
+}
--- src/main/java/tech/interview/controller/UserLDAPController.java
+++ src/main/java/tech/interview/controller/UserLDAPController.java
@@ -1,16 +1,19 @@
 package tech.interview.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import tech.interview.model.UserLDAPModel;
 import tech.interview.service.UserLDAPService;
 
 import java.util.List;
 
-@Controller
+@RestController
+@Api(value = "API REST LDAP")
+@CrossOrigin(origins = "*")
 public class UserLDAPController {
 
     private final UserLDAPService userLDAPService;
@@ -21,28 +24,33 @@ public class UserLDAPController {
     }
 
     @GetMapping("/users")
+    @ApiOperation(value = "Return all the users on the LDAP Database.")
     public ResponseEntity<List<UserLDAPModel>> findAll() {
         return new ResponseEntity<>(userLDAPService.ldapUserFindAll(), HttpStatus.OK);
     }
 
     @GetMapping("/users/{uid}")
+    @ApiOperation(value = "Return a single user uid on the LDAP Database.")
     public ResponseEntity<UserLDAPModel> findOne(@PathVariable(name = "uid") String uid) {
         return new ResponseEntity<>(userLDAPService.ldapUserFindOne(uid), HttpStatus.OK);
     }
 
     @PostMapping("/users")
+    @ApiOperation(value = "Create a User on the LDAP Database.")
     public ResponseEntity<UserLDAPModel> createUser(@RequestBody UserLDAPModel user) {
         UserLDAPModel result = userLDAPService.create(user);
         return new ResponseEntity<>(result, HttpStatus.CREATED);
     }
 
-    @PutMapping("/users")
+    @PutMapping("/users/{uid}")
+    @ApiOperation(value = "Update a User on the LDAP Database.")
     public ResponseEntity<UserLDAPModel> UpdateUser(@RequestBody UserLDAPModel user) {
         UserLDAPModel result = userLDAPService.update(user);
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
     @DeleteMapping("/users/{uid}")
+    @ApiOperation(value = "Delete a User uid on the LDAP Database.")
     public ResponseEntity<String> deleteUser(@PathVariable(name = "uid") String uid) {
         String result = userLDAPService.remove(uid);
         return new ResponseEntity<>(result, HttpStatus.OK);
